3e9c2dfaae5f5f1f2c7396837df1d53c6fe53260
Pablo <pettinarip@gmail.com>
2017-05-15 20:22:45 -0300

    add addplayerbutton component test

---
+++ src/components/AddPlayerButton/AddPlayerButton.spec.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+import renderer from 'react-test-renderer'
+import AddPlayerButton from './AddPlayerButton'
+
+describe('AddPlayerButton component', () => {
+  let addPlayer
+
+  beforeEach(() => {
+    addPlayer = jest.fn()
+  })
+
+  it('should render correctly', () => {
+    const component = renderer.create(
+      <AddPlayerButton onAddPlayer={() => null} />
+    )
+    const tree = component.toJSON()
+    expect(tree).toMatchSnapshot()
+  })
+
+  it('should call add player function when clicked', () => {
+    const component = shallow(
+      <AddPlayerButton onAddPlayer={addPlayer} />
+    )
+    component.simulate('click')
+    expect(addPlayer).toHaveBeenCalled()
+  })
+})
