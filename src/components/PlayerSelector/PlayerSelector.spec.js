3d73afa41cca799e8d476f7423586426931084d2
Pablo <pettinarip@gmail.com>
2017-05-16 13:00:43 -0300

    add PlayerSelector component test

---
+++ src/components/PlayerSelector/PlayerSelector.spec.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+
+import PlayerSelector from './PlayerSelector'
+import AddPlayerButton from '../AddPlayerButton'
+import PlayerDraggable from '../PlayerDraggable'
+
+describe('PlayerSelector component', () => {
+  const addPlayer = () => {}
+  const addPlayerPosition = () => {}
+  const removePlayerPosition = () => {}
+
+  it('should render an add player button', () => {
+    const players = []
+    const component = shallow(
+      <PlayerSelector
+        players={players}
+        onAddPlayer={addPlayer}
+        onAddPlayerPosition={addPlayerPosition}
+        onRemovePlayerPosition={removePlayerPosition}
+      />
+    )
+
+    expect(component.contains(<AddPlayerButton onAddPlayer={addPlayer} />)).toEqual(true)
+  })
+
+  it('should render all players', () => {
+    const players = [{ id: 1 }, { id: 2 }, { id: 3 }]
+    const component = shallow(
+      <PlayerSelector
+        players={players}
+        onAddPlayer={addPlayer}
+        onAddPlayerPosition={addPlayerPosition}
+        onRemovePlayerPosition={removePlayerPosition}
+      />
+    )
+
+    expect(component.find(PlayerDraggable).length).toEqual(3)
+  })
+})
