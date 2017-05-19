74beaeb6bfe24fc1a77c71f91ee130fbd6a8cc04
Pablo <pettinarip@gmail.com>
2017-05-19 01:49:25 -0300

    add fields normalization and validation

--- src/components/NewPlayerModal/NewPlayerModal.js
+++ src/components/NewPlayerModal/NewPlayerModal.js
@@ -3,6 +3,12 @@ import PropTypes from 'prop-types'
 import { Field, reduxForm } from 'redux-form'
 import Modal from '../Modal'
 
+const required = value => (value ? undefined : 'Required')
+const between = (min, max) =>
+  (value, previousValue) => value <= max && value >= min ? value : previousValue
+const maxLength = max =>
+  (value, previousValue) => value.length <= max ? value : previousValue
+
 const NewPlayerModal = props => {
   const { onSave, onRequestClose, handleSubmit, pristine, submitting } = props
   return (
@@ -14,12 +20,16 @@ const NewPlayerModal = props => {
           component='input'
           type='text'
           placeholder='Francesco Totti'
+          validate={required}
+          normalize={maxLength(30)}
         />
         <Field
           name='number'
           component='input'
           type='number'
           placeholder='Number'
+          validate={required}
+          normalize={between(1, 99)}
         />
         <button type='submit' disabled={pristine || submitting}>Agregar</button>
       </form>
