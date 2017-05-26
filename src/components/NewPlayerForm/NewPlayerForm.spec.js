a9001eb2efedc964702042c76c4def0127b32718
Pablo <pettinarip@gmail.com>
2017-05-26 20:21:53 -0300

    add new player form test

---
+++ src/components/NewPlayerForm/NewPlayerForm.spec.js
@@ -0,0 +1,49 @@
+import React from 'react'
+import { noop } from 'lodash'
+import renderer from 'react-test-renderer'
+import { mount } from 'enzyme'
+import { createStore } from 'redux'
+import {Provider} from 'react-redux'
+import { reducer as formReducer } from 'redux-form'
+
+import NewPlayerForm from './NewPlayerForm'
+
+const mockFormStore = (initialState = {}) => {
+  const store = createStore((state = initialState, action) => {
+    return {
+      ...state,
+      form: formReducer(state.form, action)
+    }
+  })
+
+  return store
+}
+
+describe('NewPlayerForm component', () => {
+  it('should render correctly', () => {
+    const store = mockFormStore()
+
+    const component = renderer.create(
+      <Provider store={store}>
+        <NewPlayerForm onSave={noop} />
+      </Provider>
+    )
+
+    const tree = component.toJSON()
+    expect(tree).toMatchSnapshot()
+  })
+
+  // it('should call onSave on submit', () => {
+  //   const store = mockFormStore()
+  //   const onSave = jest.fn()
+  //
+  //   const wrapper = mount(
+  //     <Provider store={store}>
+  //       <NewPlayerForm onSubmit={onSave} />
+  //     </Provider>
+  //   )
+  //
+  //   wrapper.find('form').simulate('submit')
+  //   expect(onSave).toHaveBeenCalled()
+  // })
+})
